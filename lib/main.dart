e37bfa16feeb9f3e6d44572c7842ad257ef0c1da
Marianne Leal <marianneleal93@gmail.de>
2021-10-23 16:50:11 +0200

    added commentary to initial main

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,25 @@
+// ignore_for_file: lines_longer_than_80_chars
+
 import 'package:flutter/material.dart';
 
 void main() {
   // 1
-  runApp(const Fooderlich());
+  runApp(const Fooderlich()); // Fooderlich = root widget
 }
 
 class Fooderlich extends StatelessWidget {
   // 2
   const Fooderlich({Key? key}) : super(key: key);
-  @override
+  @override // every stateless widget must override the build method
   Widget build(BuildContext context) {
     // TODO: Create theme
     // TODO: Apply Home widget
     // 3
-    return MaterialApp(
+    return MaterialApp( // MaterialApp = uses material design system look and feel, contains scaffold
       // TODO: Add theme
       title: 'Fooderlich',
       // 4
-      home: Scaffold(
+      home: Scaffold( // Scaffold defines layout and structure of app, has 2 properties: AppBar and Body
         // TODO: Style the title
         appBar: AppBar(title: const Text('Fooderlich'), centerTitle: true, ),
         // TODO: Style the body text
