bf94ba59bc816e713b98858dd64fb57ba54fab4f
Marianne Leal <marianneleal93@gmail.de>
2021-10-26 12:49:32 +0200

    added dark overlay, column, icon and text to card3

--- lib/card3.dart
+++ lib/card3.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: lines_longer_than_80_chars
+
 import 'package:flutter/material.dart';
 import 'fooderlich_theme.dart';
 
@@ -16,12 +18,46 @@ class Card3 extends StatelessWidget {
             image: AssetImage('assets/mag2.png'),
             fit: BoxFit.cover,
           ),
-          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          borderRadius: BorderRadius.all(Radius.circular(10.0)), //rounded corners
         ),
         child: Stack(
           children: [
-            // TODO 5: add dark overlay BoxDecoration
-            // TODO 6: Add Container, Column, Icon and Text
+            Container( // add dark overlay and rounded image corners
+              decoration: BoxDecoration(
+                // 1
+                color: Colors.black.withOpacity(0.6),
+                // 2
+                borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+              ),
+            ),
+
+            Container(
+              // 3
+              padding: const EdgeInsets.all(16), // padding all sides
+              // 4
+              child: Column(
+                // 5
+                crossAxisAlignment: CrossAxisAlignment.start, // all children on the left
+                children: [
+                  // 6
+                  const Icon(
+                    Icons.book,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                  // 7
+                  const SizedBox(height: 8), // space between text and icon
+                  // 8
+                  Text(
+                    'Recipe Trends',
+                    style: FooderlichTheme.darkTextTheme.headline2,
+                  ),
+                  // 9
+                  const SizedBox(height: 30),
+                ],
+              ),
+            ),
+
             // TODO 7: Add Center widget with Chip widget children
           ],
         ),
